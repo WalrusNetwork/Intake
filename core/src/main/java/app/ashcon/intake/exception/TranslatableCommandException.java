65e64df062ae490bf2c9a2bc5e1aca63eaff1cbb
Austin Mayes <austin@avicus.net>
2019-10-03 22:04:21 -0500

    Rebrand to Walrus

--- core/src/main/java/app/ashcon/intake/exception/TranslatableCommandException.java
+++ core/src/main/java/app/ashcon/intake/exception/TranslatableCommandException.java
@@ -1,7 +1,7 @@
 package app.ashcon.intake.exception;
 
 import app.ashcon.intake.CommandException;
-import network.stratus.common.text.PersonalizedComponent;
+import network.walrus.common.text.PersonalizedComponent;
 
 /**
  * A command exception that can be translated later on to be sent to players.
